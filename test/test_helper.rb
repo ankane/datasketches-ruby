5ecd2f4d7ac96308f1646cce1cec8c255cac09c8
Andrew Kane <andrew@ankane.org>
2021-05-17 17:49:23 -0700

    Debug test failure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
+
+class Minitest::Test
+  def setup
+    puts "\n\n#{self.class.name}##{name}"
+  end
+end
