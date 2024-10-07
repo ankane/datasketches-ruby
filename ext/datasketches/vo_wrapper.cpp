603513c4c39656a313f21aabae2f0b7c3a934ac5
Andrew Kane <andrew@ankane.org>
2024-10-07 04:22:37 -0700

    Use name [skip ci]

--- ext/datasketches/vo_wrapper.cpp
+++ ext/datasketches/vo_wrapper.cpp
@@ -8,7 +8,7 @@ using datasketches::var_opt_sketch;
 
 template<typename T>
 void bind_vo_sketch(Rice::Module &m, const char* name) {
-  Rice::define_class_under<var_opt_sketch<T>>(m, "VarOptSketch")
+  Rice::define_class_under<var_opt_sketch<T>>(m, name)
     .define_constructor(Rice::Constructor<var_opt_sketch<T>, uint32_t>())
     .define_method("k", &var_opt_sketch<T>::get_k)
     .define_method("n", &var_opt_sketch<T>::get_n)
